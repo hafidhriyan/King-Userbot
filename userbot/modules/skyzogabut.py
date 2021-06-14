7aff653aa1b1e0ee652129cdd863f582b9005d61
ridho17-ind <81844427+ridho17-ind@users.noreply.github.com>
2021-06-14 12:35:45 +0700

    Create skyzogabut.py

---
+++ userbot/modules/skyzogabut.py
@@ -0,0 +1,165 @@
+from platform import uname
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Hallo Gaes Gua Skyzo🧸...**")
+
+
+@register(outgoing=True, pattern='^.atg(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Astagfirullah...Goblokkk**!!!!")
+
+
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Yoi Bro Salken Juga Nama Gua Skyzo🗿...**")
+
+
+@register(outgoing=True, pattern='^.ast(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Astagfirullah Sayang...**")
+
+
+@register(outgoing=True, pattern='^K(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Ngontol Amat Lu Bangsat🗿**")
+
+
+@register(outgoing=True, pattern='^N(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bro? Lu Ngentot Banget🤭**")
+
+
+@register(outgoing=True, pattern='^B(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bacot Dah Lu Dasar, Goblok!!!!**")
+
+
+@register(outgoing=True, pattern='^M(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**BHAKSSSSSSSSSSSS🤣**")
+
+
+@register(outgoing=True, pattern='^Y(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**YAUDAH IYAAAAAA ASU**")
+
+
+@register(outgoing=True, pattern='^C(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MUKA LU HINA, GAUSAH SOK KERAS YA ANJENGG DASAR BABI!!**")
+
+
+@register(outgoing=True, pattern='^S(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GAUSAH SOKAP DEH LU, DASAR WIBU!!**")
+
+
+@register(outgoing=True, pattern='^V(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MACAM BAGUS AE LU BEGITU KONTOL, NGOTAK LAH BABI!!**")
+
+
+@register(outgoing=True, pattern='^J(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**JELEK BANGET LU NGENTOT, NAJIS CUIHHHH TOLOL!!!**")
+
+
+@register(outgoing=True, pattern='^A(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**PASANG PP DULU DEK,BIAR ORANG SEGRUP TAU BETAPA HINA NYA MUKA LU, UDAH TOLOL BELAGU PULA WKWKWK, MALU SAMA YANG LAIN**")
+
+@register(outgoing=True, pattern='^X(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GC SAMPAH, CUIHHHHH KEK BABI!!!!!**")
+
+
+@register(outgoing=True, pattern='^Z(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed bhakss goblok...**")
+
+
+@register(outgoing=True, pattern='^H(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Lawak Mulu ocehan lu,Ga ngena ke mental, CUIHHHH TOLOL!!!**")
+
+
+@register(outgoing=True, pattern='^O(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**JANGAN MAEN BOT MULU, ALAY LU NGENTOTT,KESANNYA NORAK GOBLOK, CUIHHHH!!!**")
+
+
+@register(outgoing=True, pattern='^G(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Gak keren lu begitu tolol, kuburan bapak lu gw gali buat dijadiin kolam renang anak paud.Cuihhhhh!!!**")
+
+CMD_HELP.update({
+    "salam":
+    "P\
+\nUsage: Untuk Memberi salam.\
+\n\nL\
+\nUsage: Untuk Menjawab Salam.\
+\n\nK\
+\nUsage: Untuk mengontoli mereka.\
+\n\nN\
+\nUsage: Kalo kesel coba aja.\
+\n\nB\
+\nUsage: Buat Ngatain Yang Suka Bacot.\
+\n\nM\
+\nUsage: Tersedak meledek.\
+\n\nY\
+\nUsage: Buat yang males adu bacot.\
+\n\nC\
+\nUsage: Buat menghujat.\
+\n\nS\
+\nUsage: Haha sokap."
+})
+
+CMD_HELP.update({
+    "salam2":
+    "V\
+\nUsage: Hujat Orang caper.\
+\n\nJ\
+\nUsage: Hujat Jamet.\
+\n\nA\
+\nUsage: Hujat yang gapunya muka.\
+\n\nX\
+\nUsage: Ngatain Grup wkwk.\
+\n\nZ\
+\nUsage: teruntuk petarung.\
+\n\nH\
+\nUsage: Coba dewek ah.\
+\n\n.atg\
+\nUsage: Istighfar 1.\
+\n\n.ast\
+\nUsage: Istighfar 2.\
+\n\nO\
+\nUsage: Ngatain org norak.\
+\n\nG\
+\nUsage: Liat Sendiri."
+})
