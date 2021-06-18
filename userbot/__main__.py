92f58c6969b85503c137aa2bb8f17c246acd0f67
ridho17-ind <81844427+ridho17-ind@users.noreply.github.com>
2021-06-18 14:48:37 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡King-Userbot⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"💠Userbot Prib💠 ⚙️ V{BOT_VER} [TELAH AKTIF GOBLOK]")
 
 
 if len(argv) not in (1, 3, 4):
